31adf8e4e802e328aa02a8c937fcb0b130e85a84
Sein07 <sangatmenggokil403@gmail.com>
2024-10-17 23:14:20 +0700

    Update DB

--- db/db_ppdbci3.sql
+++ db/db_ppdbci3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2024 at 06:02 AM
+-- Generation Time: Oct 17, 2024 at 06:12 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.1.12
 
@@ -33,13 +33,22 @@ CREATE TABLE `tb_calonsiswa` (
   `nisn` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `tgl_lahir` date NOT NULL,
-  `jk` enum('L','P') NOT NULL,
+  `jk` enum('Laki - laki','Perempuan') NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `sekolah_asal` varchar(255) NOT NULL,
   `nope` varchar(15) NOT NULL,
   `email` varchar(255) NOT NULL,
   `foto` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_calonsiswa`
+--
+
+INSERT INTO `tb_calonsiswa` (`no_daftar`, `nisn`, `nama`, `tgl_lahir`, `jk`, `alamat`, `sekolah_asal`, `nope`, `email`, `foto`) VALUES
+(12, 122222222, 'Supriyadi', '2024-10-01', 'Laki - laki', 'baleraja', 'SMPN 1 Gantar', '903894737394', 'leclerc@f1.com', '2.jpg'),
+(98, 9090, 'rusdi', '2024-10-05', 'Laki - laki', 'jalan kebenaran', 'SMPN 1 Gantar', '903894737394', 'rusdi@gmail.com', '1000568316-removebg-preview.png'),
+(99, 24122007, 'Rhadit Mika Rahil', '2007-12-24', 'Laki - laki', 'Situraja Gantar', 'SMPN 3 Gantar', '0819241323', 'rhdt.rhl@gmail.com', 'WhatsApp_Image_2024-08-22_at_19_03_25_16accef7-removebg-preview.png');
 
 -- --------------------------------------------------------
 
@@ -52,8 +61,15 @@ CREATE TABLE `tb_user` (
   `nama` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `level` enum('admin','petugas','siswa','') NOT NULL
+  `level` enum('admin','petugas','siswa','') NOT NULL DEFAULT 'petugas'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama`, `username`, `password`, `level`) VALUES
+(2, 'Rhadit Rahil', 'rhdtrhl', '$2y$10$3dnKanJlpeGvBgqIngB1SOZZqxkOVZdqed2GYx6x7M1qsPjeEaBw2', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -79,13 +95,13 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_calonsiswa`
 --
 ALTER TABLE `tb_calonsiswa`
-  MODIFY `no_daftar` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `no_daftar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
